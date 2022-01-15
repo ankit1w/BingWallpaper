76e1907c349b2c5db40fbff69e5a7266223b0c3d
Ankit Majumder <50029432+ankit1w@users.noreply.github.com>
2022-01-15 15:18:20 +0530

    Added files

---
+++ BingWallpaper.py
@@ -0,0 +1,36 @@
+import requests
+import os
+
+
+def get_wallpaper_data():
+    res = requests.get('http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1').json()
+
+    return f"http://www.bing.com{res['images'][0]['url']}", res['images'][0]['hsh']
+
+
+def hashes_differ(hsh):
+    if os.path.exists('BingWallpaper.jpg'):
+        with open('BingWallpaper.jpg', 'rb') as wall:
+            wall.seek(-32, 2)
+            new_hsh = wall.read()
+
+            if hsh.encode() == new_hsh:
+                return False
+    return True
+
+def set_wallpaper(url, hsh):
+    with open('BingWallpaper.jpg', 'wb') as wall:
+        wall.write(requests.get(url).content)
+        wall.write(hsh.encode())
+
+    os.system('WallpaperChanger.exe BingWallpaper.jpg')
+
+
+if __name__ == '__main__':
+    url, hsh = get_wallpaper_data()
+    print(hsh)
+
+    if hashes_differ(hsh):
+        print('yes')
+        set_wallpaper(url, hsh)
+        
