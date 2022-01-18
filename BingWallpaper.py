80144e0cf86bd41b0f4597362c10a172d47fd9c0
Ankit Majumder <50029432+ankit1w@users.noreply.github.com>
2022-01-18 19:33:34 +0530

    Fix directories

--- BingWallpaper.py
+++ BingWallpaper.py
@@ -1,6 +1,4 @@
-import requests
-import os
-
+import requests, os, sys
 
 def get_wallpaper_data():
     res = requests.get('http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1').json()
@@ -12,23 +10,25 @@ def hashes_differ(hsh):
     if os.path.exists('BingWallpaper.jpg'):
         with open('BingWallpaper.jpg', 'rb') as wall:
             wall.seek(-32, 2)
-            new_hsh = wall.read()
+            old_hsh = wall.read()
 
-            if hsh.encode() == new_hsh:
+            if hsh == old_hsh.decode():
                 return False
     return True
+
 
 def set_wallpaper(url, hsh):
     with open('BingWallpaper.jpg', 'wb') as wall:
         wall.write(requests.get(url).content)
         wall.write(hsh.encode())
 
-    os.system('WallpaperChanger.exe BingWallpaper.jpg')
+    os.popen('WallpaperChanger.exe BingWallpaper.jpg')
 
 
 if __name__ == '__main__':
+    os.chdir(os.path.dirname(sys.argv[0]))
+    
     url, hsh = get_wallpaper_data()
 
     if hashes_differ(hsh):
         set_wallpaper(url, hsh)
-        
