04f771398a1808e1c20785510d8335642de01d34
Ankit Majumder <50029432+ankit1w@users.noreply.github.com>
2022-02-07 17:29:53 +0530

    Update BingWallpaper.py

--- BingWallpaper.py
+++ BingWallpaper.py
@@ -1,33 +1,36 @@
-import requests, os, sys
+from requests import get
+import os
+
 
 def get_wallpaper_data():
-    res = requests.get('http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1').json()
+    res = get(f'{base_url}HPImageArchive.aspx?format=js&n=1').json()
 
-    return f"http://www.bing.com{res['images'][0]['url']}", res['images'][0]['hsh']
+    return res['images'][0]['url'], res['images'][0]['hsh']
 
 
 def hashes_differ(hsh):
     if os.path.exists('BingWallpaper.jpg'):
         with open('BingWallpaper.jpg', 'rb') as wall:
             wall.seek(-32, 2)
-            old_hsh = wall.read()
+            old_hsh = wall.read().decode()
 
-            if hsh == old_hsh.decode():
+            if hsh == old_hsh:
                 return False
     return True
 
 
 def set_wallpaper(url, hsh):
     with open('BingWallpaper.jpg', 'wb') as wall:
-        wall.write(requests.get(url).content)
+        wall.write(get(base_url+url).content)
         wall.write(hsh.encode())
 
     os.popen('WallpaperChanger.exe BingWallpaper.jpg')
 
 
 if __name__ == '__main__':
-    os.chdir(os.path.dirname(sys.argv[0]))
-    
+    base_url = 'http://www.bing.com/'
+    os.chdir(os.path.dirname(__file__))
+
     url, hsh = get_wallpaper_data()
 
     if hashes_differ(hsh):
